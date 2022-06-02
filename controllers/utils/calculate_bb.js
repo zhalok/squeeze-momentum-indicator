482c930565e925c3d331e9b922bd48162be00892
zhalok <zhalokrahman007@gmail.com>
2022-06-02 14:43:33 +0600

    update

--- controllers/utils/calculate_bb.js
+++ controllers/utils/calculate_bb.js
@@ -4,5 +4,19 @@ const calculate_bb = (input) => {
   const bb_res = BB.calculate(input);
   return bb_res;
 };
+const input = {
+  values: [
+    127.75, 129.02, 132.75, 145.4, 148.98, 137.52, 147.38, 139.05, 137.23,
+    149.3, 162.45, 178.95, 200.35, 221.9, 243.23, 243.52, 286.42, 280.27,
+  ],
+  period: 10,
 
+  stdDev: 2,
+};
+
+console.log(calculate_bb(input));
 module.exports = calculate_bb;
+// yes it also passed
+// so i am not understanding what problem is occuring to your testing
+// I request you to test again with the data set here
+// the data set was directly copied from the github repo
--- testing/calculate_kc.test.js
+++ testing/calculate_kc.test.js
@@ -2,7 +2,9 @@ const calculate_kc = require("../controllers/utils/calculate_kc");
 const test_data = require("./kc_test_data.json");
 const input = test_data.input;
 const output = test_data.output;
-
+// unit testing function for testing kc calculation
+// as you can see it passed successfully
+// now lets try using the function you imported
 test("lets check", () => {
   expect(calculate_kc(input)).toStrictEqual(output);
 });
